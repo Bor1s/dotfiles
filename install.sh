cf90d439606c2387cb6f9980d9678dcb2c7c9c5c
Borys Babusenko <borys.babusenko@shopify.com>
2021-06-30 14:18:38 +0200

    Install Plug vim in the system

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 mkdir ~/.config/nvim/
 ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
