3dfe04d44cdc6a242fc03cc34832fcd8adf0a466
Borys <boris.bbk@gmail.com>
2024-02-10 10:45:21 +0100

    Add lazyvim config

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,5 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.netrw_winsize = 25
+vim.g.netrw_keepdir = 0
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { "RRethy/nvim-treesitter-endwise" },
+    opts = function(_, opts)
+      opts.endwise = { enable = true }
+    end,
+  },
+}
