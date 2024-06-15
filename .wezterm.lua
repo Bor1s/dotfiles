60e0646c8c61ffeaa0c02b33b54fa4b036fbca6d
Borys <boris.bbk@gmail.com>
2024-06-15 19:08:37 +0200

    Decrease background opacity for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -18,7 +18,7 @@ config.hide_tab_bar_if_only_one_tab = true
 config.color_scheme = "Tokyo Night"
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.8
 config.macos_window_background_blur = 8
 
 -- Background image settings
