b80f5e11a4011c4488476e7cd6b5fc9ad913e1a1
Borys Babusenko <borys.babusenko@on-running.com>
2024-11-19 11:40:33 +0100

    Updated dashboard-nvim config

--- nvim/lua/plugins/dashboard-nvim.lua
+++ nvim/lua/plugins/dashboard-nvim.lua
@@ -1,47 +1,51 @@
 return {
   {
     "nvimdev/dashboard-nvim",
-    opts = function(_, opts)
-      opts.theme = "hyper"
-      opts.config.week_header = {
-        enable = true,
-      }
-
-      opts.config.project = { enable = false }
-      opts.config.mru = { limit = 3, cwd_only = false }
-
-      opts.config.shortcut = {
-        {
-          action = "ene",
-          desc = " New file",
-          icon = " ",
-          key = "n",
+    event = "VimEnter",
+    config = function()
+      require("dashboard").setup({
+        theme = "hyper",
+        config = {
+          week_header = {
+            enable = true,
+          },
+          project = { enable = false },
+          mru = { limit = 3, cwd_only = false },
+          shortcut = {
+            {
+              action = "ene",
+              desc = " New file",
+              icon = " ",
+              key = "n",
+            },
+            {
+              action = [[lua require("lazyvim.util").telescope.config_files()()]],
+              desc = " Config",
+              icon = " ",
+              key = "c",
+            },
+            {
+              action = "LazyExtras",
+              desc = " Lazy Extras",
+              icon = " ",
+              key = "x",
+            },
+            {
+              action = "Lazy",
+              desc = " Lazy",
+              icon = "󰒲 ",
+              key = "l",
+            },
+            {
+              action = "qa",
+              desc = " Quit",
+              icon = " ",
+              key = "q",
+            },
+          },
         },
-        {
-          action = [[lua require("lazyvim.util").telescope.config_files()()]],
-          desc = " Config",
-          icon = " ",
-          key = "c",
-        },
-        {
-          action = "LazyExtras",
-          desc = " Lazy Extras",
-          icon = " ",
-          key = "x",
-        },
-        {
-          action = "Lazy",
-          desc = " Lazy",
-          icon = "󰒲 ",
-          key = "l",
-        },
-        {
-          action = "qa",
-          desc = " Quit",
-          icon = " ",
-          key = "q",
-        },
-      }
+      })
     end,
+    dependencies = { { "nvim-tree/nvim-web-devicons" } },
   },
 }
